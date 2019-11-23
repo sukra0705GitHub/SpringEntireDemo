3ee4d9cc0dfa037647294a009afea6a4c46e249e
sukra0705 <409922354@qq.com>
2019-11-23 15:38:12 +0800

    Configure DispatcherServlet with Java Code

--- src/JavaCodeConfigureSample/MyWebApplicationInitializer.java
+++ src/JavaCodeConfigureSample/MyWebApplicationInitializer.java
@@ -3,9 +3,12 @@ package JavaCodeConfigureSample;
 import SpringConfigFile.SpringConfigByJavaCode;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 public class MyWebApplicationInitializer implements WebApplicationInitializer {
     @Override
@@ -16,6 +19,9 @@ public class MyWebApplicationInitializer implements WebApplicationInitializer {
         annotationConfigApplicationContext.refresh();
 
         // Create and register the DispatcherServlet
-        
+        DispatcherServlet dispatcherServlet  = new DispatcherServlet((WebApplicationContext) annotationConfigApplicationContext);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("sukraApp", dispatcherServlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/sukraApp/*");
     }
 }
